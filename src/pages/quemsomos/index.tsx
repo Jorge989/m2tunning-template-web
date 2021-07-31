2fee258b5a048ec29c85077efb7447d2ffe55876
Jorge989 <attiejorge89@gmail.com>
2021-07-31 08:51:23 -0300

    vai

--- src/pages/quemsomos/index.tsx
+++ src/pages/quemsomos/index.tsx
@@ -7,40 +7,38 @@ import Footer from '../../components/footer'
  import { Container,   Wrapper, ImageBox, Textos } from './styles'
 
  
-const Quemsomos: React.FC = () => {
-    return (
-      <>
-        <Header></Header>
-        <HeaderMobile />
-        <Container>
-          <Wrapper>
-            <Textos>
-              <h1>Sobre Nós</h1>
-              <p className="paragrafo-desktop">
-                Somos apaixonados por customização e envelopamento com 07 anos
-                de experiência na arte de envelopamento. A M2 Tunning nasceu da
-                vontade de levar aquilo que já faziamos dentro de casa para
-                outras pessoas a fazer parte da cosntrução de momentos e
-                experiências diferenciadas que irão marcar as vidas de nossos
-                cliente e amigos.
-              </p>
-              <p className="paragrafo-mobile">
-                Somos apaixonados por customização e envelopamento com 07 anos
-                de experiência na arte de envelopamento. A M2 Tunning nasceu da
-                vontade de levar aquilo que já faziamos dentro de casa para
-                outras pessoas a fazer parte da cosntrução de momentos e
-                experiências diferenciadas que irão marcar as vidas de nossos
-                cliente e amigos.
-              </p>
-            </Textos>
-            <ImageBox>
-              <img className="imagem" src={m2} alt="logo" />
-            </ImageBox>
-          </Wrapper>
+
+export function Quemsomos() {
+  return (
+    <>
+      <Header></Header>
+      <HeaderMobile />
+      <Container>
+        <Wrapper>
+          <Textos>
+            <h1>Sobre Nós</h1>
+            <p className="paragrafo-desktop">
+              Somos apaixonados por customização e envelopamento com 07 anos de
+              experiência na arte de envelopamento. A M2 Tunning nasceu da
+              vontade de levar aquilo que já faziamos dentro de casa para outras
+              pessoas a fazer parte da cosntrução de momentos e experiências
+              diferenciadas que irão marcar as vidas de nossos cliente e amigos.
+            </p>
+            <p className="paragrafo-mobile">
+              Somos apaixonados por customização e envelopamento com 07 anos de
+              experiência na arte de envelopamento. A M2 Tunning nasceu da
+              vontade de levar aquilo que já faziamos dentro de casa para outras
+              pessoas a fazer parte da cosntrução de momentos e experiências
+              diferenciadas que irão marcar as vidas de nossos cliente e amigos.
+            </p>
+          </Textos>
+          <ImageBox>
+            <img className="imagem" src={m2} alt="logo" />
+          </ImageBox>
+        </Wrapper>
         <Footer />
-        </Container>
-      </>
-    )
+      </Container>
+    </>
+  )
 }
 
-export default Quemsomos
